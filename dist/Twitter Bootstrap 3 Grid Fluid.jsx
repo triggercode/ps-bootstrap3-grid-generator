1170a1210a55135f10c17582500898c4d0b25957
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-09-18 19:18:18 +0200

    Adds support for fluid grids (width = 100% of active document)

--- dist/Twitter Bootstrap 3 Grid Fluid.jsx
+++ dist/Twitter Bootstrap 3 Grid Fluid.jsx
@@ -100,7 +100,6 @@ if (typeof module !== "undefined" && module !== null) {
 }
 
 draw_grid({
-  width: grid_definitions.grid_desktop_width,
   columns: grid_definitions.grid_number_of_columns,
   gutter: grid_definitions.grid_half_gutter
 });
---
+++ src/fluid.coffee
@@ -0,0 +1,3 @@
+draw_grid
+  columns: grid_definitions.grid_number_of_columns
+  gutter:  grid_definitions.grid_half_gutter
